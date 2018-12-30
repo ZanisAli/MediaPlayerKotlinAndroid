4bc2016169dab4702a89b000c002fc480abc5c23
Zanis Ali <zanisali999@gmail.com>
2018-12-30 17:45:55 +0100

    songs from storage implemented

--- app/src/main/java/startup/softflix/com/startup/MainActivity.kt
+++ app/src/main/java/startup/softflix/com/startup/MainActivity.kt
@@ -1,11 +1,16 @@
 package startup.softflix.com.startup
 
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.song_ticket.view.*
 
@@ -24,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        LoadURLOnline()
-        adapter=MySongAdapter(listSongs)
-        lsListSongs.adapter=adapter
+        //LoadURLOnline()
+        CheckUserPermsions()
+        
 
         var mytracking= mySongTrack()
         mytracking.start()
@@ -112,6 +117,61 @@ class MainActivity : AppCompatActivity() {
                }
            }
 
+        }
+    }
+
+
+    fun CheckUserPermsions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_CODE_ASK_PERMISSIONS)
+                return
+            }
+        }
+
+        LoadSong()
+
+    }
+
+    //get acces to location permsion
+    private val REQUEST_CODE_ASK_PERMISSIONS = 123
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE_ASK_PERMISSIONS -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                LoadSong()
+            } else {
+                // Permission Denied
+                Toast.makeText(this, "denail", Toast.LENGTH_SHORT)
+                        .show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    fun   LoadSong() {
+        val allSongsURI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val selection = MediaStore.Audio.Media.IS_MUSIC + "!=0"
+        val cursor = contentResolver.query(allSongsURI, null, selection, null, null)
+        if (cursor != null) {
+            if (cursor!!.moveToFirst()) {
+
+                do {
+
+                    val songURL = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.Media.DATA))
+                    val SongAuthor = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.Media.ARTIST))
+                    val SongName = cursor!!.getString(cursor!!.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME))
+                    listSongs.add(SongInfo(SongName, SongAuthor, songURL))
+                } while (cursor!!.moveToNext())
+
+
+            }
+            cursor!!.close()
+
+            adapter=MySongAdapter(listSongs)
+            lsListSongs.adapter=adapter
         }
     }
 }
